6bbc41038e877dce12ccafc22e36df6ed4592950
schully80 <edr.schulter@gmail.com>
2026-01-20 13:11:52 +0200

    Commit and deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
 
+    trailingSlash: "ignore",
+
+
   vite: {
     define: {
       'process.env': process.env
