8639bb4d74a416525fc0466d5104a511e3285b7d
schully80 <edr.schulter@gmail.com>
2026-01-20 15:23:51 +0200

    updated our values page. added hero image

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,6 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
 
-    trailingSlash: "ignore",
-
-
   vite: {
     define: {
       'process.env': process.env
