9e7b8077d0bd1c54d0a40a202ba7718e4fcbeb02
schully80 <edr.schulter@gmail.com>
2026-01-21 11:53:01 +0200

    Made changes to astro.config.mjs. added sitemap settings. also visit page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from "@astrojs/sitemap";
+
 
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://thelifeplace.org",
+  integrations: [sitemap()],
+  integrations:[tailwind()],
 
   vite: {
     define: {
